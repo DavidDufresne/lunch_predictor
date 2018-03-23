242ea691bb30ba4aa334ce6bc9eb124156983492
David Dufresne <dufresne@mojotech.com>
2018-03-23 14:23:51 -0400

    Add generated context and model for providers.

---
+++ lib/lunch_predictor/providers/provider.ex
@@ -0,0 +1,18 @@
+defmodule LunchPredictor.Providers.Provider do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "providers" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(provider, attrs) do
+    provider
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
