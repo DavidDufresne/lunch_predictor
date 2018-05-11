11d2ada6997dd631888903b0543341cf8263bfd6
David Dufresne <dufresne@mojotech.com>
2018-05-11 15:55:39 -0400

    Add date display.

--- lib/lunch_predictor_web/controllers/page_controller.ex
+++ lib/lunch_predictor_web/controllers/page_controller.ex
@@ -4,10 +4,21 @@ defmodule LunchPredictorWeb.PageController do
   alias LunchPredictor.Lunches
   alias LunchPredictor.Statistics
 
+  @english_day_names %{
+    0 => "Sunday",
+    1 => "Monday",
+    2 => "Tuesday",
+    3 => "Wednesday",
+    4 => "Thursday",
+    5 => "Friday",
+    6 => "Saturday"
+  }
+
   def index(conn, _params) do
     providers = Lunches.list_providers()
     priors = Enum.map(providers, fn(provider) -> Statistics.prior(provider.id) end)
     providers_with_priors = Enum.zip(providers, priors)
-    render conn, "index.html", providers_with_priors: providers_with_priors
+    day_of_week = @english_day_names[Date.day_of_week(Date.utc_today)]
+    render conn, "index.html", providers_with_priors: providers_with_priors, day_of_week: day_of_week
   end
 end
